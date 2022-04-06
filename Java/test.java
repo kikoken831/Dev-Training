da6ac0f2776b00c40df454c7e92452fe06c5f06a
kikoken831 <kennyhu831@gmail.com>
2022-04-06 10:07:13 +0800

    day 2 commit

--- Java/test.java
+++ Java/test.java
@@ -1,7 +1,26 @@
 package Java;
-public class test
-{
+
+public class test {
     public static void main(String[] args) {
-        System.out.println("Test1");
+        int a[][] = new int[3][];
+        //2 sch 
+        //
+        a[0] = new int[4];
+        a[1] = new int[3];
+        a[2] = new int[5];
+        int count = 0;
+        for (int i = 0; i < a.length; i++) {// populate
+            for (int j = 0; j < a[i].length; j++) {
+                a[i][j] = count++;
+
+            }
+        }
+        for (int i = 0; i < a.length; i++) {// print
+            for (int j = 0; j < a[i].length; j++) {
+
+                System.out.println("The ID of student from class: " + i + " is class index: " + j + " is " + a[i][j]);
+
+            }
+        }
     }
 }
